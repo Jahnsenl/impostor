742e05971dd3bd3fe4efe649818a1543888b0f2c
Jahnsen <jjahnsenl@gmail.com>
2026-07-01 15:49:28 +0200

    Fix TS errors for Vercel build

--- client/src/context/GameContext.tsx
+++ client/src/context/GameContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import type { ReactNode } from 'react';
 import { io, Socket } from 'socket.io-client';
 import type { GameState } from '../types/game';
